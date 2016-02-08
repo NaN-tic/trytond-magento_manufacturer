39fcf672523ada6f35a5f6b34f6415c7a9296c2f
resteve <resteve@zikzakmedia.com>
2016-02-08 16:12:36 +0100

    Remove Base External Mapping

---
+++ product.py
@@ -0,0 +1,41 @@
+# This file is part magento_weight module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool, PoolMeta
+
+__all__ = ['Product']
+__metaclass__ = PoolMeta
+
+
+class Product:
+    __name__ = "product.product"
+
+    @classmethod
+    def magento_import_product(cls, values, shop=None):
+        MagentoManufacturer = Pool().get('magento.manufacturer')
+
+        vals = super(Product, cls).magento_import_product(values, shop)
+
+        manufacturer = values.get('manufacturer')
+        manufacturers = MagentoManufacturer.search([
+            ('value','=', manufacturer),
+            ], limit=1)
+    
+        if manufacturers:
+            mgn_manufacturer, = manufacturers
+            vals['manufacturer'] = mgn_manufacturer.manufacturer
+        return vals
+
+    @classmethod
+    def magento_export_product(cls, app, product, shop=None, lang='en_US'):
+        MagentoManufacturer = Pool().get('magento.manufacturer')
+
+        vals = super(Product, cls).magento_export_product(app, product, shop, lang)
+
+        if product.manufacturer:
+            manus = MagentoManufacturer.search([
+                ('manufacturer', '=', product.manufacturer),
+                ], limit=1)
+            if manus:
+                vals['manufacturer'] = manus[0].value
+        return vals
