c326b97afd1044f517a7e93684a040dbb29015d6
resteve <resteve@zikzakmedia.com>
2016-02-08 16:12:36 +0100

    Remove Base External Mapping

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # This file is part magento_manufacturer module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
 from trytond.pool import Pool
 from .magento_core import *
-
+from .product import *
 
 def register():
     Pool.register(
         MagentoApp,
         MagentoManufacturer,
+        Product,
         module='magento_manufacturer', type_='model')
